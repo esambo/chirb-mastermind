fd35661db58aa5fedd4304cd112e04b1f9e51010
E. Sambo <news@emmanuelsambo.com>
2010-06-23 08:15:16 -0500

    Refactor before :each to use let

--- spec/mastermind/game_spec.rb
+++ spec/mastermind/game_spec.rb
@@ -2,20 +2,18 @@ require File.join(File.dirname(__FILE__), "/../spec_helper")
 
 module Mastermind
   describe Game do
-    context "starting up" do
-      before(:each) do
-        @messenger = mock("messenger").as_null_object
-        @game = Game.new(@messenger)
-      end
+    describe "start" do
+      let(:messenger) { mock("messenger").as_null_object }
+      let(:game)      { Game.new(messenger) }
 
       it "should send a welcome message" do
-        @messenger.should_receive(:puts).with("Welcome to Mastermind!")
-        @game.start(%w[a b c d])
+        messenger.should_receive(:puts).with("Welcome to Mastermind!")
+        game.start(%w[a b c d])
       end
 
       it "should prompt for the first guess" do
-        @messenger.should_receive(:puts).with("Enter guess:")
-        @game.start(%w[a b c d])
+        messenger.should_receive(:puts).with("Enter guess:")
+        game.start(%w[a b c d])
       end
     end
   end
