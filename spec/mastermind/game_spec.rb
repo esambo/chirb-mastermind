cee1e424f1dd846471d46c310a9f469a43172713
E. Sambo <news@emmanuelsambo.com>
2011-01-04 21:13:27 -0600

    Clean up strings using single quotes and use "#" to describe methods

--- spec/mastermind/game_spec.rb
+++ spec/mastermind/game_spec.rb
@@ -5,21 +5,21 @@ module Mastermind
     let(:messenger) { mock("messenger").as_null_object }
     let(:game)      { Game.new(messenger) }
 
-    describe "start" do
-      it "should send a welcome message" do
+    describe '#start' do
+      it 'sends a welcome message' do
         messenger.should_receive(:puts).with("Welcome to Mastermind!")
         game.start(%w[a b c d])
       end
 
-      it "should prompt for the first guess" do
+      it 'prompt for the first guess' do
         messenger.should_receive(:puts).with("Enter guess:")
         game.start(%w[a b c d])
       end
     end
 
-    describe "guess" do
-      context "0 correct" do
-        it "matches nothing" do
+    describe '#guess' do
+      context '0 correct' do
+        it 'matches nothing' do
           messenger.should_receive(:puts).with('')
           game.start(%w[r g y c])
           game.guess(%w[w w w w])
