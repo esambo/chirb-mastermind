69bdcd925dfe0a88fbcf6130380de9dd59f5de0f
E. Sambo <news@emmanuelsambo.com>
2011-01-04 23:19:59 -0600

    Refactor: use #reduce instead of #each and combine the local variable mark in #guess

--- lib/mastermind/game.rb
+++ lib/mastermind/game.rb
@@ -11,33 +11,23 @@ module Mastermind
     end
     
     def guess(guess)
-      mark = ''
-
       position_matches = number_of_position_matches(guess)
-      mark += 'b' * position_matches
-      
       any_matches = number_of_any_color_matches(guess)
-      mark += 'w' * (any_matches - position_matches)
-
+      mark = 'b' * position_matches + 
+             'w' * (any_matches - position_matches)
       @messenger.puts mark
     end
     
     def number_of_position_matches(guess)
-      sum = 0
-      (0..@secret.count - 1).each do |i|
-        sum += 1 if @secret[i] == guess[i]
+      (0..@secret.count - 1).reduce(0) do |sum, e|
+        sum + (@secret[e] == guess[e] ? 1 : 0)
       end
-      sum
     end
     
     def number_of_any_color_matches(guess)
-      any_matches = 0
-      guess.each do |e|
-        if @secret.include?(e)
-          any_matches += 1
-        end
+      guess.reduce(0) do |sum, e|
+        sum + (@secret.include?(e) ? 1 : 0)
       end
-      any_matches
     end
   end
 end
